edcbfbdc6c1bd3570d803ec49a55bc2d5661e706
jeffreyshe <jeffreyshe@jeffreyshe-PC>
2012-10-24 14:55:44 +0800

    Add comment

--- Hw1Test/src/ArrayTest.java
+++ Hw1Test/src/ArrayTest.java
@@ -1,22 +1,30 @@
+/*
+作者:Jeffreyshe
+簡單描述:對MyArray做unit test
+更新日期:2012/10/24
+*/
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTest {
 	private MyArray array;
-	private static final double DELTA = 0.000001;  /*double型態之expected和result相差多少視為相等*/
+	private static final double DELTA = 0.000001;  //double型態之expected和result相差多少視為相等
 	
+	/*test開始前會執行的函式*/
 	@Before
 	public void setUp()
 	{
 		array=new MyArray("1,2,3,4,5,6,7,8,9,10");
 	}
 	
+	/*test完後執行的函式*/
 	@After
 	public void tearDown()
 	{
 		array=null;
 	}
 	
+	/*測試array中的最大值*/
 	@Test
 	public void testMaximum()
 	{
@@ -25,6 +33,7 @@ public class ArrayTest {
 		assertEquals(expected, result,DELTA);
 	}
 	
+	/*測試array中的最小值*/
 	@Test
 	public void testMinimum()
 	{
@@ -33,6 +42,7 @@ public class ArrayTest {
 		assertEquals(expected, result,DELTA);
 	}
 	
+	/*測試array中的平均值*/
 	@Test
 	public void testMean()
 	{
@@ -41,6 +51,7 @@ public class ArrayTest {
 		assertEquals(expected, result,DELTA);
 	}
 	
+	/*測試array中的中位數*/
 	@Test
 	public void testMedian()
 	{
@@ -49,6 +60,7 @@ public class ArrayTest {
 		assertEquals(expected, result,DELTA);
 	}
 	
+	/*測試array的變異數*/
 	@Test
 	public void testVariance()
 	{
@@ -57,6 +69,7 @@ public class ArrayTest {
 		assertEquals(expected, result,DELTA);
 	}
 	
+	/*測試array的標準差*/
 	@Test
 	public void testStandardDeviation()
 	{
