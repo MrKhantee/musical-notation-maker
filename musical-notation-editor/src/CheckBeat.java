445f4ca8e35fa8395679e1973f84a705e262853b
jeffreyshe <jeffreyshe@jeffreyshe-PC>
2012-11-09 21:25:08 +0800

    新增unit testing & ant 建構文件

--- musical-notation-editor/src/CheckBeat.java
+++ musical-notation-editor/src/CheckBeat.java
@@ -14,45 +14,56 @@ public class CheckBeat
 {
 	private static String[] Input_String = new String[10000];//行數最大值:10000
 	private static int InputIndex;
+	private String FilePath;
+	public Float TotalBeat;
 	/*讀取song資料夾下所有檔案*/
-	private static void ReadFile() throws IOException
+	
+	public CheckBeat(String Path)
 	{
-		File dir = new File("./song");//當前目錄的"song"目錄下的相對路徑
-		String[] filenames = dir.list();//列出所有附錄下檔名
-		for(int i=0;i<filenames.length;i++)
+		FilePath=Path;
+		try
 		{
-			System.out.println("檔名:"+filenames[i]);
-			FileReader fr = new FileReader("./song/"+filenames[i]);  
+			ReadFile();
+		}
+		catch(Exception e)	{}
+
+	}
+	
+	private void ReadFile() throws IOException
+	{
+			FileReader fr = new FileReader(FilePath);  
 	        BufferedReader br=new BufferedReader(fr);  
-	        String line;  
+	        String Line;  
 	        InputIndex = 0;
-	        while((line = br.readLine()) != null)//讀取每一行
+	        while((Line = br.readLine()) != null)//讀取每一行
 	        {  
-	            Input_String[InputIndex] = line;//存入Input_String Array
+	            Input_String[InputIndex] = Line;//存入Input_String Array
 	            InputIndex++;
 	        }  
 	        br.close();  
 	        Process(); 
-		}
+
 	}
 	/*從檔案分割出每一小節*/
-	private static void Process()
+	private void Process()
 	{
 		String[] FirstRow = Input_String[0].split("/");
 		String[] Section = new String[10];
-		int sectionBeat = Integer.valueOf(FirstRow[0]).intValue();//每一音檔中規定每一小節應該要幾拍
-		int standard = Integer.valueOf(FirstRow[1]).intValue();//每一音檔規定幾分音符是一拍
-		int sectionId = 1;
+		int CorrectSectionBeat = Integer.valueOf(FirstRow[0]).intValue();//每一音檔中規定每一小節應該要幾拍
+		int Standard = Integer.valueOf(FirstRow[1]).intValue();//每一音檔規定幾分音符是一拍
+		int SectionId = 1;
+		float TotalBeat=0;
 		/*抓出小節*/
 		for(int i=1 ; i<InputIndex ; i++)
 		{
 			Section = Input_String[i].split("\\|");
-			int howManySection;
-			howManySection = Section.length;
-			for(int j=1;j<=howManySection-1;j++)
+			int HowManySection;
+			HowManySection = Section.length;
+			for(int j=1;j<=HowManySection-1;j++)
 			{
-				float  Beat = Check(Section[j],standard);
-				System.out.println("節拍數:"+Beat+" "+"小節數:"+sectionId+" 標準節拍數:"+sectionBeat);//印出是音檔中哪一小節和算出的節拍數
+				float  Beat = ReturnSectionBeat(Section[j],Standard);
+				TotalBeat=TotalBeat+Beat;
+				//System.out.println("節拍數:"+Beat+" "+"小節數:"+SectionId+" 標準節拍數:"+CorrectSectionBeat);//印出是音檔中哪一小節和算出的節拍數
 				/*
 				 可以在這邊check每一音檔每一小節是否有節拍錯誤 
 				if(sectionBeat == Beat)  
@@ -60,61 +71,57 @@ public class CheckBeat
 					正確!!!
 				}  
 				*/
-				sectionId++;
+				SectionId++;
 			}
 		}
+		this.TotalBeat=new Float(TotalBeat);
 	}
 	/*計算每一小節節拍數*/
-	private static float Check(String Section,int standerd)
+	private float ReturnSectionBeat(String Section,int standerd)
 	{
-		Pattern pattern2;
-        Matcher matcher2;
-        int note = 0;//這是幾分音符
-        float delay = 0;//這是多少延音
-        float ansBeat = 0;//最後那一小節的節拍數
+		Pattern Pattern2;
+        Matcher Matcher2;
+        int Note = 0;//這是幾分音符
+        float Delay = 0;//這是多少延音
+        float AnsBeat = 0;//最後那一小節的節拍數
 		//System.out.println(Section);
 		Pattern pattern = Pattern.compile("\\([#|b]??,.*?,.*?\\)");//(給自己的註解:match最短    p.s括號跟|都算特殊字元)
-        Matcher matcher = pattern.matcher(Section);
-        while (matcher.find())
+        Matcher Matcher = pattern.matcher(Section);
+        while (Matcher.find())
         {
             String Section1;
-            Section1 = matcher.group();
-            pattern2 = Pattern.compile("\\d+");
-            matcher2 = pattern2.matcher(Section1);
-            int hasSecond = 0;//看有沒有延音的變數
-            while (matcher2.find())
+            Section1 = Matcher.group();
+            Pattern2 = Pattern.compile("\\d+");
+            Matcher2 = Pattern2.matcher(Section1);
+            int HasSecond = 0;//看有沒有延音的變數
+            while (Matcher2.find())
             { 
-                if(hasSecond == 0)
+                if(HasSecond == 0)
                 {
-                	note = Integer.valueOf(matcher2.group());
+                	Note = Integer.valueOf(Matcher2.group());
                 }
                 else
                 {
-                	delay = Integer.valueOf(matcher2.group());
+                	Delay = Integer.valueOf(Matcher2.group());
                 }
-                hasSecond++;
+                HasSecond++;
             }
-            float Rbeat = (float)note/standerd;//節拍的倒數
-            float beat = 1/Rbeat;//節拍
-            if(hasSecond == 1)
+            float Rbeat = (float)Note/standerd;//節拍的倒數
+            float Beat = 1/Rbeat;//節拍
+            if(HasSecond == 1)
             {
-            	ansBeat += beat+delay;//把每一小節的節拍加起來
+            	AnsBeat += Beat+Delay;//把每一小節的節拍加起來
             }
             else
             {
-            	ansBeat += beat;//如果有延音,加上去
+            	AnsBeat += Beat;//如果有延音,加上去
             }
         }
-		return ansBeat;
+		return AnsBeat;
 	}
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException 
-	{
-		// TODO Auto-generated method stub
-		ReadFile(); 
-	}
 
 }
